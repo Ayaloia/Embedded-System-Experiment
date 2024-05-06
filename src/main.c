572f46d7333e8bb57a09bb29be97a8d141c07102
aldlss <ayaldlss@gmail.com>
2024-05-06 14:51:27 +0800

    init

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include"bsp_mem_map.h"
+#include"psp_api.h"
+// #include "rtthread.h"
+
+#define GPIO_SWs        0x80001400
+#define GPIO_LEDs       0x80001404
+#define SegEn_ADDR      0x80001038
+#define SegDig_ADDR     0x8000103C
+
+int main() {
+    // rt_kprintf("111");
+}
